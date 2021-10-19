72dc7ccbd4cd4c7cf9520c473897ec569a4f9716
mohidkhan87 <mohidkhan87@gmail.com>
2021-10-19 18:17:48 +0500

    dynamic field and form

--- vendors/js/index.js
+++ vendors/js/index.js
@@ -60,8 +60,7 @@ $("#demo").steps({
     // step4
     if (currentIndex === 3) {
       if (stepDirection === "forward") {
-        recitalsFormValidator.resetForm();
-        return recitalsForm.valid();
+        // return recitalsForm.valid();
       }
       if (stepDirection === "backward") {
         // recitalsFormValidator.resetForm();
@@ -82,9 +81,6 @@ $("#demo").steps({
     alert("Agreement Completed");
   },
 });
-
-
-
 
 function toggleForm(name) {
   switch (name) {
@@ -129,6 +125,12 @@ function changeDisplay(currentStep) {
   }
 }
 
+// Step 3
+function loanHandler(event){
+  let id = event.target.id;
+  document.getElementById('display'+id).innerHTML = ' '+event.target.value;
+}
+
 // Step 4
 let recitalsTarget = $("form#recitals-form");
 let recitals = 1;
@@ -160,3 +162,42 @@ function recitalsInputHandler(event){
   document.getElementById("display-recitals-input-" + id).innerHTML = eventValue;
 }
 showInputs();
+
+// Step 5
+let definitionTarget = $("#dynamic-definition-forms");
+let definitionForms = 1;
+function add_definition_form() {
+  definitionForms++;
+  let form = document.createElement("form");
+  form.setAttribute("id", "definition-form-" + definitionForms);
+  form.innerHTML = '<hr><p>Definitions</p><input type="text" onKeyUp="definitionInputHandler(event)" id="definition-input-'+definitionForms+'" placeholder="Definitions" name="definition" required /><p>Meaning</p><input type="text" onKeyUp="definitionInputHandler(event)" id="meaning-input-'+definitionForms+'" placeholder="Meaning" name="meaning" required /><button class="removebtn" type="button" onclick="remove_definition_form('+definitionForms+')">Delete <i class="fa fa-minus"></i></button>'
+  definitionTarget.append(form);
+  showDefinitionInputs()
+}
+function remove_definition_form(id) {
+  definitionForms--;
+  $('#definition-form-' + id).remove();
+  showDefinitionInputs()
+}
+function showDefinitionInputs() {
+  let numberOfForms = $('#dynamic-definition-forms').children().length;;
+  console.log(numberOfForms);
+  let input = '';
+  for (var i = 0; i < numberOfForms; i++) {
+    input += '<div class="form-text"><div class="form-table"><p class="definition" id="display-definition-input-' + (i+1) + '">INPUT Definition</p><p class="meaning" id="display-meaning-input-' + (i+1) + '">INPUT Meaning</p></div></div>'
+  }
+  let target = $(".display-definition-inputs");
+  target.html(input);
+}
+function definitionInputHandler(event){
+  let type = event.target.id.substring(0,7)
+  let eventValue = event.target.value;
+  if(type === "meaning"){
+    let id = event.target.id.substring(14)
+    document.getElementById("display-meaning-input-" + id).innerHTML = eventValue;
+  }else{
+    let id = event.target.id.substring(17)
+    document.getElementById("display-definition-input-" + id).innerHTML = eventValue;
+  }
+}
+showDefinitionInputs();
